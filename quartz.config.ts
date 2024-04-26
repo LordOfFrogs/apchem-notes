eb1c7533ae1d340c19656036269cd54797395160
Tori <ndtech260@gmail.com>
2024-04-26 14:04:07 -0400

    Quartz sync: Apr 26, 2024, 2:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "lordoffrogs.github.io/apchem-notes",
+    baseUrl: "lordoffrogs.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
